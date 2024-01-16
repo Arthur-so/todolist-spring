f6ad9c06d6b954ff9151b1b80bb3aa458efc41d2
Arthur-so <arthursantos.oliveira001@gmail.com>
2024-01-16 18:25:06 -0300

    Pegando autenticação do filtro

--- src/main/java/br/com/arthuroliveira/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/arthuroliveira/todolist/filter/FilterTaskAuth.java
@@ -1,23 +1,37 @@
 package br.com.arthuroliveira.todolist.filter;
 
 import java.io.IOException;
+import java.util.Base64;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
-import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Component
-public class FilterTaskAuth implements Filter {
+public class FilterTaskAuth extends OncePerRequestFilter {
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse reponse, FilterChain chain)
-            throws IOException, ServletException {
-        System.out.println("Chegou no filtro.");
-        chain.doFilter(request, reponse);
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        
+                // Pegar a autenticação (usuário e senha)
+                String authorization = request.getHeader("Authorization");
+                String authEncoded =  authorization.substring("Basic".length()).trim();
+                byte[] authDecoded =  Base64.getDecoder().decode(authEncoded);
+                String authString = new String(authDecoded);
+
+                String[] credentials = authString.split(":");
+                String username = credentials[0];
+                String password = credentials[1];
+
+                System.out.println(username);
+                System.out.println(password);
+
+                filterChain.doFilter(request, response);
     }
     
 }
