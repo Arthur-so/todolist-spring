7272b76b85881038d69ed5520d4feed896368330
Arthur-so <arthursantos.oliveira001@gmail.com>
2024-01-16 11:14:35 -0300

    Criação de tarefas.

---
+++ src/main/java/br/com/arthuroliveira/todolist/task/ITaskRepository.java
@@ -0,0 +1,9 @@
+package br.com.arthuroliveira.todolist.task;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {
+    
+}
